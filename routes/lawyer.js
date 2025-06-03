26c20cfa8c5b53129eb02ce208d1477b1507eb7d
Tananan Narkchuay <zackalocloud@gmail.com>
2025-06-04 02:13:42 +0700

    fix upload

--- routes/lawyer.js
+++ routes/lawyer.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const multer = require('multer');
 const { protect, authorize } = require('../middleware/auth');
 const { addLawyer, getLawyerById, updateLawyer, deleteLawyer, getAllLawyers,changeVerifyStatus } = require('../controllers/lawyer');
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 const router = express.Router();
 
-router.route('/').post(protect, authorize('lawyer'), addLawyer).get(getAllLawyers);
+router.route('/').post(protect, authorize('lawyer'), upload.single('image'),addLawyer).get(getAllLawyers);
 
-router.route('/:id').get(getLawyerById).put(protect, authorize('lawyer'), updateLawyer).delete(protect, authorize('lawyer'), deleteLawyer);
+router.route('/:id').get(getLawyerById).put(protect, authorize('lawyer'), upload.single('image'),updateLawyer).delete(protect, authorize('lawyer'),upload.single('image'), deleteLawyer);
 
 router.route('/:id/status').put(protect, authorize('admin'), changeVerifyStatus);
 
