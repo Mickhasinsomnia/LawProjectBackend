bcfc1f0c4a067b8a4289bb46a5692f7d131004de
Tananan Narkchuay <zackalocloud@gmail.com>
2025-06-06 16:12:58 +0700

    news now have image

--- controllers/news.js
+++ controllers/news.js
@@ -1,8 +1,20 @@
 const News = require("../models/News");
-
+const {
+  generateFileName,
+  uploadFile,
+  getObjectSignedUrl,
+  deleteFile,
+} = require("./s3.js");
 
 exports.createNews = async (req, res) => {
   try {
+
+    if (req.file) {
+      const imageName = generateFileName();
+      await uploadFile(req.file, imageName, req.file.mimetype);
+      req.body.image = imageName;
+    }
+
     const newNews = await News.create({
       ...req.body,
       poster_id: req.user.id,
@@ -24,6 +36,14 @@ exports.createNews = async (req, res) => {
 exports.getAllNews = async (req, res) => {
   try {
     const news = await News.find().populate("poster_id", "name");
+
+    for (const some of news) {
+
+      if (some.image && !some.image.startsWith("http")) {
+        some.image = await getObjectSignedUrl(some.image);
+      }
+    }
+
     res.status(200).json({ success: true, data: news });
   } catch (err) {
     res
@@ -44,6 +64,10 @@ exports.getNews = async (req, res) => {
       return res
         .status(404)
         .json({ success: false, message: "News not found" });
+
+    if (news.image && !news.image.startsWith("http")) {
+      news.image = await getObjectSignedUrl(news.image);
+    }
 
     res.status(200).json({ success: true, data: news });
   } catch (err) {
--- routes/news.js
+++ routes/news.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { createNews, getAllNews, getNews, updateNews, deleteNews } = require('../controllers/news');
 const { protect, authorize } = require('../middleware/auth');
+const multer = require('multer');
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 const router = express.Router();
 
 router.get('/', getAllNews);
 router.get('/:id', getNews);
-router.post('/', protect, authorize('admin'), createNews);
+router.post('/', protect, authorize('admin'),upload.single('image'), createNews);
 router.put('/:id', protect, authorize('admin'), updateNews);
 router.delete('/:id', protect, authorize('admin'), deleteNews);
 
