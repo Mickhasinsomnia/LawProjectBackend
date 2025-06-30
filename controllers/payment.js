9395e3ea29b9ae0117847b36b2ed9fdeab3ef335
Tananan Narkchuay <zackalocloud@gmail.com>
2025-06-30 17:34:40 +0700

    beta payment

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,5 +1,7 @@
 const Payment = require('../models/Payment');
 const Hiring = require('../models/Hiring');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
 
 
 exports.addPayment = async (req, res) => {
@@ -49,16 +51,45 @@ exports.getPayment = async (req, res) => {
   }
 }
 
-exports.payVerify = async (req,res) =>{
+exports.payVerify = async (req, res) => {
+  try {
 
-  const bill = await Payment.findById(req.params.id);
+    const bill = await Payment.findById(req.params.id);
+    if (!bill) {
+      return res.status(404).json({ success: false, message: 'Payment not found' });
+    }
 
-  if(!bill){
-    return res.status(404).json({success:false , message: 'Payment not found',})
+    if (!req.file) {
+      return res.status(401).json({ success: false, message: 'Please upload slip' });
+    }
+
+    const formData = new FormData();
+    formData.append('file', req.file.buffer, req.file.originalname);
+
+
+    const response = await fetch(process.env.SLIP_API, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.SLIP_TOKEN}`,
+        ...formData.getHeaders(),
+      },
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (response.status === 200) {
+
+      bill.status = 'paid';
+      await bill.save();
+
+      return res.status(200).json({ success: true, message: 'Payment verified and updated', data });
+    } else {
+      return res.status(response.status).json({ success: false, message: 'Verification failed', data });
+    }
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: 'Server error' });
   }
-
-
-
-
-
-}
+};
--- routes/payment.js
+++ routes/payment.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { addPayment ,getPayment } = require('../controllers/payment');
+const { addPayment ,getPayment,payVerify } = require('../controllers/payment');
 const { protect, authorize } = require('../middleware/auth');
-
+const multer = require('multer');
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 const router = express.Router();
 
 router.post('/create/:id',protect, authorize('lawyer'), addPayment);
 router.get('/',protect,getPayment)
+router.post('/:id',upload.single('file'),payVerify)
 
 module.exports = router;
