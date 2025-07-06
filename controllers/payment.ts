7641ce3662d47aac6e538302eb431687c8e1f12f
Tananan Narkchuay <zackalocloud@gmail.com>
2025-07-06 20:38:28 +0700

    payment complete

--- controllers/payment.ts
+++ controllers/payment.ts
@@ -121,8 +121,59 @@ export const handlePayment = async (req: Request, res: Response, next: NextFunct
       success_url: 'https://example.com/success',
       cancel_url: 'https://example.com/cancel',
     });
+
+    bill.stripeSession_id = session.id;
+    await bill.save();
+
     res.status(200).json({ url: session.url });
   } catch (error) {
     next(error);
+  }
+};
+
+export const verifyPayment = async (req: Request, res: Response) => {
+  try {
+
+
+    const bill = await Payment.findById(req.params.id);
+    if (!bill) {
+      res.status(404).json({ success: false, message: 'Payment not found' });
+      return;
+    }
+
+    const session_id = bill.stripeSession_id;
+
+    if (!session_id || typeof session_id !== 'string') {
+      res.status(400).json({ error: 'Missing or invalid session_id' });
+      return;
+    }
+
+    const key = process.env.STRIPE_SECRET_KEY;
+
+    if (!key) {
+      res.status(500).json({ error: 'Stripe API key is not configured' });
+      return;
+    }
+
+    const stripe = new Stripe(key);
+
+    const session = await stripe.checkout.sessions.retrieve(session_id);
+
+    if (session.payment_status === 'paid') {
+
+      bill.status = 'paid';
+
+      await bill.save();
+
+      res.status(200).json({ success: true, message: 'Payment completed' });
+      return;
+    } else {
+      res.status(200).json({ success: false, message: 'Payment not completed' });
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+    return;
   }
 };
--- routes/payment.ts
+++ routes/payment.ts
@@ -1,12 +1,13 @@
 import express from 'express';
-import { addPayment, getPayment,handlePayment } from '../controllers/payment.js';
+import { addPayment, getPayment,handlePayment,verifyPayment } from '../controllers/payment.js';
 import { protect, authorize } from '../middleware/auth.js';
 
 
 const router = express.Router();
 
-router.post('/create/:id', protect, authorize('lawyer'), addPayment);
+router.post('/create/:id', protect, authorize('lawyer','admin'), addPayment);
 router.get('/', protect, getPayment);
-router.post('/checkout/:id', handlePayment);
+router.post('/checkout/:id', protect,handlePayment);
+router.post('/checkout/:id/verify',protect,verifyPayment)
 
 export default router;
